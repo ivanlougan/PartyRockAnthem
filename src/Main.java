81ddb209d53b85de052a918b6b97ef3109b0ec1a
Robert Zdeb <robinio123@gmail.com>
2020-10-20 05:08:55 +0200

    Collecting user details to create a list of guest, meals and phone numbers

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean shouldContinue = true;
+
+        final Party party = new Party();
+
+        while (shouldContinue) {
+            System.out.println("Wybierz opcję");
+            System.out.println("1. Wyświetl gości");
+            System.out.println("2. Dodaj gościa");
+            System.out.println("3. Wyświetl potrawy");
+            System.out.println("4. Znajdź po id");
+            System.out.println("5. Coś tam");
+            System.out.println("6. Wyjście");
+
+
+            int userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+                case 1:
+                    party.displayGuests();
+                    break;
+                case 2:
+                    party.addGuest();
+                    break;
+                case 3:
+                    party.displayMeals();
+                    break;
+                case 4:
+                    party.displayGuestByPhoneNumber();
+                    break;
+                case 5:
+                    System.out.println("wybrano 5");
+                    break;
+                case 6:
+                    shouldContinue = false;
+                    break;
+        }
+        }
+    }
+}
---
+++ src/Party.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Party {
+    private List<Guest> guests = new ArrayList<>();
+    private Set<String> meals = new HashSet<>();
+    private Map<Integer, Guest> phoneToGuest = new HashMap<>();
+    Scanner scanner = new Scanner(System.in);
+
+    public void addGuest() {
+        System.out.println("podaj imię gościa");
+        String name = scanner.nextLine();
+        System.out.println("podaj preferowany posiłek");
+        String meal = scanner.nextLine();
+        System.out.println("podaj numer telefonu");
+        int phoneNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Czy jesteś weganinem? (y/n)");
+        String isVeganString = scanner.nextLine();
+
+        boolean isVegan;
+
+        if (isVeganString.equals("y"))
+            isVegan = true;
+        else {
+            isVegan = false;
+        }
+
+        Guest guest = new Guest(name, meal, phoneNumber, isVegan);
+
+        meals.add(meal);
+        phoneToGuest.put(phoneNumber, guest);
+        guests.add(guest);
+    }
+
+    public void displayMeals(){
+        for (String meal : meals) {
+            System.out.println(meal);
+        }
+    }
+
+    public void displayGuestByPhoneNumber() {
+        System.out.println("podaj numer telefonu");
+        Integer phoneNumber = Integer.valueOf(scanner.nextLine());
+        Guest guest = phoneToGuest.get(phoneNumber);
+
+        guest.displayGuestInfrmation();
+    }
+
+    public void displayGuests() {
+        for (Guest guest : guests) {
+            guest.displayGuestInfrmation();
+            System.out.println("");
+        }
+    }
+}
